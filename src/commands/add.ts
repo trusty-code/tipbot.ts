5f2b1af0bda0a2a8bb4d1203210fb6feac235595
huhn511 <sehe89@gmail.com>
2020-06-30 00:31:15 +0200

    eat: add command

--- src/commands/add.ts
+++ src/commands/add.ts
@@ -1,10 +1,15 @@
 import { isValidChecksum } from '@iota/checksum';
+import { setAddress } from '../database';
 
 export function add(user: string, address: string) {
   try {
     // Check if address has valid checksum
-    if (isValidChecksum(address)) return 'Address added.';
-    return 'Error - Checksum is missing.';
+    if (isValidChecksum(address)) {
+        setAddress(user, address)
+        return 'Address added.';
+    } else {
+        return 'Error - Checksum is missing.';
+    }
   } catch (error) {
     return 'Error - Probably Not A Valid IOTA Address';
   }
---
+++ src/database.ts
@@ -0,0 +1,27 @@
+const Keyv = require('keyv')
+const KeyvFile = require('keyv-file').KeyvFile
+const path = require('path');
+
+const keyv = new Keyv({
+  store: new KeyvFile()
+});
+// More options with default value:
+const customKeyv = new Keyv({
+  store: new KeyvFile({
+    filename: `${Math.random().toString(36).slice(2)}.json`, // the file path to store the data
+    expiredCheckDelay: 24 * 3600 * 1000, // ms, check and remove expired data in each ms
+    writeDelay: 100, // ms, batch write to disk in a specific duration, enhance write performance.
+    encode: JSON.stringify, // serialize function
+    decode: JSON.parse // deserialize function
+  })
+})
+
+
+export async function setAddress(user_id: string, address: string) {
+    await keyv.set(user_id, address)
+}
+
+export async function getAddress(user_id: string) {
+    let address = await keyv.get(user_id)
+    return address;
+}
