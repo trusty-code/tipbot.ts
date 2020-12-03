4df31d7ad7332785cbdbc03f36a071b4c0862db9
huhn511 <sehe89@gmail.com>
2020-12-03 22:00:11 +0100

    fix del method

--- src/commands/delete.ts
+++ src/commands/delete.ts
@@ -1,17 +1,16 @@
 import { deleteUser, getAddress } from '../database';
 
 export function del(user: string) {
-    try {
-        // Get address from user
-        let address = getAddress(user)
-        if(address) {
-          deleteUser(user)
-          return 'Account successfully deleted!'
-        } else {
-          return 'Error - User not found'
-        }
-      } catch (error) {
-        return 'Error - Something wild appears.';
-      }
+  try {
+    // Get address from user
+    let address = getAddress(user)
+    if (address) {
+      deleteUser(user)
+      return 'Account successfully deleted!'
+    } else {
+      return 'Error - User not found'
     }
+  } catch (error) {
+    return 'Error - Something wild appears.';
+  }
 }
