7ff7622e121d24c3603cff259d7db9d07cb9043b
NO8ODY <44928989+no8ody@users.noreply.github.com>
2020-12-04 21:42:58 +0100

    fix promise with async func

--- src/commands/delete.ts
+++ src/commands/delete.ts
@@ -1,14 +1,14 @@
 import { deleteUser, getAddress } from '../database';
 
-export function del(user: string) {
+async export function del(user: string) {
   try {
     // Get address from user
-    let address = getAddress(user)
-    if (address) {
+    let address = await getAddress(user)
+    if (typeof address !== 'undefined' && address) {
       deleteUser(user)
       return 'Account successfully deleted!'
     } else {
-      return 'Error - User not found'
+      return 'User not found!'
     }
   } catch (error) {
     return 'Error - Something wild appears.';
