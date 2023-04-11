b84267c3d29b4c43919163e4abfd5223bdd464c0
Anton Krinner <krinneranton@googlemail.com>
2023-04-11 14:56:36 +0200

    Adjust and enhance tests for add command

--- tests/Add.test.ts
+++ tests/Add.test.ts
@@ -1,13 +1,21 @@
-import { add } from '../src/index';
-test('Add Valid Address', () => {
-  expect(add('Carl', 'HYMYMBXAWOTQSSCMUHLTEZM9ODBRQHQXE9YYITUIQKTLMIKHMUTSETAQIFRCNKIVFNEETSXDNHAYHYSVCHPXUVQDMX')).toBe('Address added.');
+import {add} from '../src';
+
+test('Adding a valid address and user', () => {
+    expect(add('Carl', 'iota1qqqa5f3a04dj5lazwn6fgsjpx9rclqszafs7wkqft7lpxq65am37wrzgnjy')).toBe('Address added.');
 });
 
-test('Try to Add without Checksum', () => {
-  expect(add('Carl', 'HYMYMBXAWOTQSSCMUHLTEZM9ODBRQHQXE9YYITUIQKTLMIKHMUTSETAQIFRCNKIVFNEETSXDNHAYHYSVC')).toBe("Error - Missing checksum.")
+test('Trying to add a valid address without prefix', () => {
+    expect(add('Carl', 'qqqa5f3a04dj5lazwn6fgsjpx9rclqszafs7wkqft7lpxq65am37wrzgnjy')).toBe("Error - Address is missing or not valid.")
 });
 
-test('Try to Add Invalid Address', () => {
-  expect(add('Carl', 'INVALID')).toMatch(/(Error)/i)
+test('Trying to add an invalid address', () => {
+    expect(add('Carl', 'INVALID')).toBe('Error - Address is missing or not valid.')
+});
 
+test('Trying to add an empty address', () => {
+    expect(add('Carl', '')).toBe('Error - Address is missing or not valid.')
+});
+
+test('Trying to add a valid address without user', () => {
+    expect(add('', 'iota1qqqa5f3a04dj5lazwn6fgsjpx9rclqszafs7wkqft7lpxq65am37wrzgnjy')).toBe('Error - Missing user.')
 });
